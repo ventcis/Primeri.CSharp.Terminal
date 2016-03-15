53cf2b44c0d8063035698d695a61ee8abb95ba91
ventcis <ventcis@abv.bg>
2016-03-15 08:11:46 +0200

    Модул 02, Видео 07 - Работа с масиви

---
+++ Zadachki/Zadachki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadachki
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Деклариране на променливи
+			string a = "Hello", b = "Live";
+			object c = (a + " " + b);
+			string d = (string)c;
+
+			Console.WriteLine ("Изписва променлива от тип object: " + c + "\n");
+			Console.WriteLine ("{0} {1}", a, b);
+
+
+
+		
+		}
+	}
+}
